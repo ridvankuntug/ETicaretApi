7b7287304ca8b1fe971a6a14d7fc8005912c72e9
Rıdvan KÜNTUĞ <ridvankuntug@gmail.com>
2022-03-29 16:03:50 +0300

    Add project files.

---
+++ Core/ETicaretApi.Domain/Entities/Order.cs
@@ -0,0 +1,20 @@
+﻿using ETicaretApi.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretApi.Domain.Entities
+{
+    public class Order : BaseEntity
+    {
+        public string Description { get; set; }
+        public string Address { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+
+        public Guid CustomerId { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
